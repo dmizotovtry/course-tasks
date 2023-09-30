849fafc455c5c454653eca788ea6093084dbf031
Dmitry Zotov <100532533+dmizotovtry@users.noreply.github.com>
2023-10-01 00:00:08 +0500

    calculator

---
+++ calculator.py
@@ -0,0 +1,19 @@
+a = float(input())
+b = float(input())
+c = str(input())
+if (c == '/' or c == 'mod' or c == 'div') and (b == 0):
+  print('Деление на 0!')
+elif c == '+':
+  print(a + b)
+elif c == '-':
+  print(a - b)
+elif c == '*':
+  print(a * b)
+elif c == 'pow':
+  print(a ** b)
+elif c == '/':
+  print(a / b)
+elif c == 'mod':
+  print(a % b)
+elif c == 'div':
+  print(a // b)
