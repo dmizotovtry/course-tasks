0a5e69c43649d564f7c767a6fd34878476f9fbb4
Dmitry Zotov <100532533+dmizotovtry@users.noreply.github.com>
2023-11-05 22:11:23 +0500

    area of figures

---
+++ areaOfFigures.py
@@ -0,0 +1,16 @@
+type = str(input())
+if type == "triangle":
+  a = float(input())
+  b = float(input())
+  c = float(input())
+  p = float((a + b + c) / 2)
+  print((p * (p - a) * (p - b) * (p - c)) ** 0.5)
+elif type == "rectangle":
+  a = int(input())
+  b = int(input())
+  print(a * b)
+elif type == "circle":
+  r = float(input())
+  print(3.14 * r ** 2)
+else:
+  print("Incorrect value")
